3e1c4a0c51c87dbfec08dcaba9a766aaf43c4dbf
rcrawf13 <richmondcrawford44@gmail.com>
2025-10-14 13:56:48 -0400

    Added other Components/Tweaked existing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import AnimationBar from "./components/AnimationBar"
 import CodePen from "./components/codepen/CodePen"
 import ArtBoard from "./components/artboard/ArtBoard"
+import ToolBar from "./components/toolbar/ToolBar"
 function App() {
 
   return (
     <>
-    <CodePen/>
-    <ArtBoard/>
-    <AnimationBar/>
+    <div className="grid">
+      <div className="columns">
+        
+        <CodePen/>
+        <ArtBoard/>
+        <ToolBar/>
+      </div>
+        <AnimationBar/>
+    </div>
      
     
     </>
---
+++ src/components/toolbar/ToolBar.tsx
@@ -0,0 +1,16 @@
+import "./toolbar.css"
+const ShapeBar = () => {
+
+
+    
+    return (
+        <div className="toolbar-Container">
+            <div className="square"></div>
+            <div className="circle"></div>
+            <div className="triangle"></div>
+        </div>
+    )
+}
+
+
+export default ShapeBar
