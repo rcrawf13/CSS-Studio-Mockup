54ff833707b4c33ab9ab21fb2d38f9d094c4a666
rcrawf13 <richmondcrawford44@gmail.com>
2025-10-20 08:08:44 -0400

    added react-konva to project

--- src/components/artboard/ArtBoard.jsx
+++ src/components/artboard/ArtBoard.jsx
@@ -1,10 +1,20 @@
-import "./artboard.css"
+import { Stage, Layer, Circle } from "react-konva";
+
 const ArtBoard = () => {
   return (
-    <div className="artboard">
+    <Stage width={500} height={500}>
+      <Layer>
+        <Circle
+          x={250}
+          y={250}
+          radius={100}
+          fill="red"
+          stroke="black"
+          strokeWidth={4}
+        />
+      </Layer>
+    </Stage>
+  );
+};
 
-    </div>
-  )
-}
-
-export default ArtBoard
+export default ArtBoard;
--- src/components/toolbar/ToolBar.tsx
+++ src/components/toolbar/ToolBar.tsx
@@ -1,13 +1,18 @@
 import "./toolbar.css"
+import { useState } from "react"
 const ShapeBar = () => {
 
-
+    const [currentTool, setCurrentTool] = useState("");
     
+    const ChangeCurrentTool = (toolName:string) => {
+        setCurrentTool(toolName);        
+    } 
+
     return (
         <div className="toolbar-Container">
-            <div className="square"></div>
-            <div className="circle"></div>
-            <div className="triangle"></div>
+            <div style={{filter:currentTool === "square"?"brightness(50%)":""}} onClick={()=>{ChangeCurrentTool("square")}} className="square"></div>
+            <div style={{filter:currentTool === "circle"?"brightness(50%)":""}} onClick={()=>{ChangeCurrentTool("circle")}} className="circle"></div>
+            <div style={{filter:currentTool === "triangle"?"brightness(50%)":""}} onClick={()=>{ChangeCurrentTool("triangle")}} className="triangle"></div>
         </div>
     )
 }
